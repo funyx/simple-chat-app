5932683a4a6f7b07a738c8e917f17bdc743a2bca
Dimitar Dobchev <ddobchev@gmail.com>
2016-09-16 14:08:38 +0300

    improved

--- src/app/_services/app.service.ts
+++ src/app/_services/app.service.ts
@@ -8,38 +8,24 @@ export type InteralStateType = {
 @Injectable()
 export class AppState {
   _state: InteralStateType = { };
-
   constructor(
     private _storage : Storage
-  ) {
-
-  }
-
-  // already return a clone of the current state
+  ){}
   get state() {
     return this._state = this._clone(this._state);
   }
-  // never allow mutation
   set state(value) {
     throw new Error('do not mutate the `.state` directly');
   }
-
-
   get(prop?: any) {
-    // use our state getter for the clone
     const state = this.state;
     return state.hasOwnProperty(prop) ? state[prop] : (this._storage.get(`${prop}`) ? this._storage.get(`${prop}`) : false);
   }
-
   set(prop: string, value: any) {
-    // internally mutate our state
     this._storage.save(`${prop}`,value);
     return this._state[prop] = value;
   }
-
-
   private _clone(object: InteralStateType) {
-    // simple object clone
-    return JSON.parse(JSON.stringify( object ));
+    return Object.assign({},object);
   }
 }
